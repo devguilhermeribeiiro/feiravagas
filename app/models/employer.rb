01e46288d98be960100229daf327d35dbdcdb4f9
devguilhermeribeiro <devguilhermeribeiro000@gmail.com>
2025-04-15 00:02:32 -0300

    Creating Employer devise resource

---
+++ app/models/employer.rb
@@ -0,0 +1,7 @@
+class Employer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable
+end
