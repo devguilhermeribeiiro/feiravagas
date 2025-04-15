70f7ff167a3b7c9e315e7d32699c8bd91d4d49d0
devguilhermeribeiro <devguilhermeribeiro000@gmail.com>
2025-04-15 00:46:45 -0300

    Generating Devise Controllers for Job Seekers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :job_seekers
+  # Defines the root path route ("/")
+  # root "posts#index"
+
+  devise_for :job_seekers, controllers: {
+    registrations: "job_seekers/registrations"
+  }
+
   devise_for :employers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -10,7 +16,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
