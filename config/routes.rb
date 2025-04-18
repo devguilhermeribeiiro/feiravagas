a7498f651f5a3ab14cc0519e54297d4b08775543
devguilhermeribeiro <devguilhermeribeiro000@gmail.com>
2025-04-18 20:07:09 -0300

    Moving devise resources to top

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :job_seekers, controllers: {
+    registrations: "job_seekers/registrations"
+  }
+
+  devise_for :employers, controllers: {
+    registrations: "employers/registrations"
+  }
+
   resources :employers, only: %i[show edit update destroy] do
     resources :jobs
     collection do
@@ -18,21 +26,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
 
-  devise_for :job_seekers, controllers: {
-    registrations: "job_seekers/registrations"
-  }
-
-  devise_for :employers, controllers: {
-    registrations: "employers/registrations"
-  }
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 end
