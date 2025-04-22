acba35ccbf6606c20a9a74b2bbd93e871d66c38c
devguilhermeribeiro <devguilhermeribeiro000@gmail.com>
2025-04-22 14:48:46 -0300

    setup after login path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(JobSeeker)
+      jobseeker_index_path(resource)
+    elsif resource.is_a?(Employer)
+      dashboard_jobs_path(resource)
+    else
+      root_path
+    end
+  end
 end
